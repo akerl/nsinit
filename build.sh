8ef011cf0a09783e56526e30c2e6c399bb18d18b
Les Aker <me@lesaker.org>
2014-06-12 20:37:15 -0400

    I suppose static is overkill :P

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir -p gopath/{src,pkg,bin}
 export GOPATH="$(pwd)/gopath"
 
 go get github.com/docker/libcontainer/nsinit
-go install --ldflags '-extldflags "-static"' github.com/docker/libcontainer/nsinit
+go install github.com/docker/libcontainer/nsinit
 
 cp gopath/src/github.com/docker/libcontainer/LICENSE .
 cp gopath/src/github.com/docker/libcontainer/NOTICE .
