3099b383bfbc6acf20a033c39d38ffa1d9207e7e
Les Aker <me@lesaker.org>
2014-06-12 20:46:30 -0400

    update packages on rebuild

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ cd $(dirname $0)
 mkdir -p gopath/{src,pkg,bin}
 export GOPATH="$(pwd)/gopath"
 
-go get github.com/docker/libcontainer/nsinit
-go install github.com/docker/libcontainer/nsinit
+go get -u github.com/docker/libcontainer/nsinit
+go install -n github.com/docker/libcontainer/nsinit
 
 cp gopath/src/github.com/docker/libcontainer/LICENSE .
 cp gopath/src/github.com/docker/libcontainer/NOTICE .
